f42fe35c8317031322ba853cb76510fa645c5ba6
Sonam Yadav <yadavsonam23032007@gmail.com>
2026-09-15 14:34:58 +0530

    http-server

---
+++ http_server.js
@@ -0,0 +1,12 @@
+const http=require(`http`);
+const PORT=3000;
+const server=http.createServer((req,res)=>{
+  console.log(`Request received: ${req.method} ${req.url}`);
+  res.statusCode=200;
+  res.setHeader('Content-Type','text/plain');
+  res.setHeader('X-Powered-By','Node.js');
+  res.end('Hello, World!\n');
+});
+server.listen(PORT, ()=>{
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
